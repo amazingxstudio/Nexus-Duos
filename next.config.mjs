67f9694cb13c2a29b03dc9353918fc95fc4108dc
amazingxstudio <amazingxstudio@gmail.com>
2026-08-09 15:38:47 +0630

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "t.me" },
+      { protocol: "https", hostname: "**.telegram.org" },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
